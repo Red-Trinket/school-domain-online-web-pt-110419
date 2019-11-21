06d930abadb9f9de0d8771d6848249260d1b93e0
Matthew Jones <matts3371@gmail.com>
2019-11-21 23:16:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,15 @@
 # code here!
 class School
   
+  attr_accessor :name 
   
+  ROSTER = {} 
+  
+  def add_student(name, grade)
+    ROSTER[grade] = []
+    ROSTER[grade] << name
+  
+  def initialize(name)
+    @name = name 
   
 end
